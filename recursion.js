40737b0fe06405ae6565c290b81b8160f514ad69
Hannah Thorpe <hthorpe@protonmail.com>
2020-06-08 11:03:35 -0400

    initial commit

---
+++ recursion.js
@@ -0,0 +1,7 @@
+// recursion drills checkpoint
+
+// recursion is a concept
+// recursive algorith has two parts (base/general case)
+// base case is when to stop
+// general case is the function calling itself
+
